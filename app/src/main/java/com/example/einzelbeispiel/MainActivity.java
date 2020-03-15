537752c6e538fa8a589c40fd3563e3864f2fb017
Amar Karabegovic <amarka@edu.aau.at>
2020-03-15 14:38:11 +0100

    Fixed UI, implemented 2.2 functionality.

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button calculateButton = (Button) findViewById(R.id.buttonCalculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateDivisors();
+            }
+        });
+
         Button sendButton = (Button) findViewById(R.id.button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +37,35 @@ public class MainActivity extends AppCompatActivity {
                 sendData();
             }
         });
+    }
+
+    public void calculateDivisors() {
+        EditText matrikelNummer = (EditText) findViewById(R.id.Matrikelnummer);
+        TextView displayResult = (TextView) findViewById(R.id.textView4);
+        String mNummer = matrikelNummer.getText().toString();
+        int length = mNummer.length();
+
+        int[] arr = new int[length];
+
+        for(int i=0;i<length;i++) {
+            arr[i] = Character.getNumericValue(mNummer.charAt(i));
+        }
+
+        int num1 = arr[0];
+        int num2 = arr[1];
+        int gcd = 1;
+
+        for(int i = 1; i <= num1 && i <= num2; i++)
+        {
+            if(num1%i==0 && num2%i==0)
+                gcd = i;
+        }
+
+        if (gcd > 1) {
+            displayResult.setText("The indexes of the numbers are 1 and 2");
+    } else {
+            displayResult.setText("Nichts");
+        }
     }
 
 
