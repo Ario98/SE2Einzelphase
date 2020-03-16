85a96af92be95426d26fc482e22e9917d9d94757
Amar Karabegovic <amarka@edu.aau.at>
2020-03-16 14:58:35 +0100

    Fixed UI, implemented 2.2 functionality.

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.einzelbeispiel;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,18 +8,22 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView displayResult = (TextView) findViewById(R.id.textView3);
 
         Button calculateButton = (Button) findViewById(R.id.buttonCalculate);
         calculateButton.setOnClickListener(new View.OnClickListener() {
@@ -34,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendData();
+                String recievedInput = dataExchange();
+                displayResult.setText(recievedInput);
             }
         });
     }
+
 
     public void calculateDivisors() {
         EditText matrikelNummer = (EditText) findViewById(R.id.Matrikelnummer);
@@ -47,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         int[] arr = new int[length];
 
-        for(int i=0;i<length;i++) {
+        for (int i = 0; i < length; i++) {
             arr[i] = Character.getNumericValue(mNummer.charAt(i));
         }
 
@@ -55,49 +60,56 @@ public class MainActivity extends AppCompatActivity {
         int num2 = arr[1];
         int gcd = 1;
 
-        for(int i = 1; i <= num1 && i <= num2; i++)
-        {
-            if(num1%i==0 && num2%i==0)
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0)
                 gcd = i;
         }
 
         if (gcd > 1) {
             displayResult.setText("The indexes of the numbers are 1 and 2");
-    } else {
+        } else {
             displayResult.setText("Nichts");
         }
     }
 
+    String serverResponse;
 
-    public void sendData() {
-        EditText matrikelNummer = (EditText) findViewById(R.id.Matrikelnummer);
-        String output = matrikelNummer.getText().toString();
+    private String dataExchange() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-        BackgroundTask b1 = new BackgroundTask();
-        b1.execute(output);
+                try {
+                    EditText matrikelNummer = (EditText) findViewById(R.id.Matrikelnummer); //two times declared-change
 
-    }
+                    Socket s = new Socket("se2-isys.aau.at", 53212);
+                    OutputStream output = s.getOutputStream();
 
-    class BackgroundTask extends AsyncTask<String,Void,Void> {
+                    PrintWriter printWriter = new PrintWriter(output, true);
+                    printWriter.println(matrikelNummer.getText().toString());
 
-        Socket s;
-        PrintWriter writer;
+                    //answer from server
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    serverResponse = bufferedReader.readLine();
 
-        @Override
-        protected Void doInBackground(String... voids) {
+
+                    //close and release system resources
+                    printWriter.close();
+                    output.close();
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
         try {
-            String output = voids[0];
-            s = new Socket("se2-isys.aau.at", 53212);
-            writer = new PrintWriter(s.getOutputStream());
-            writer.write(output);
-            writer.flush();
-            writer.close();
-
-        } catch (IOException e) {
+            thread.join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        return null;
-        }
+        return serverResponse;
     }
 }
